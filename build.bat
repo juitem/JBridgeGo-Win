eccb7dbe6a2e7af7844130aa26f6917afa8f4bb7
김준우/Tizen Platform Lab(SR)/삼성전자 <jw1772.kim@samsung.com>
2026-05-28 16:45:52 +0900

    Fix Windows build and packaging

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@ setlocal
 cd /d "%~dp0"
 call npm.cmd install
 if errorlevel 1 exit /b %errorlevel%
-call npm.cmd run dev
+call npm.cmd run build:win
+if errorlevel 1 exit /b %errorlevel%
+echo Artifacts: release\
 endlocal
